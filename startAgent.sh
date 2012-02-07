461d30d8b016f91cf5545f40e6508dde5337b583
Kjetil Valle <kjetil.valle@bekk.no>
2012-02-07 20:29:30 +0100

    fjernet hardkoding av plasseringen til scenariene fra startAgent

--- startAgent.sh
+++ startAgent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm log/*
-java -cp lib/json-20080701.jar:lib/grinder.jar net.grinder.Grinder scenarier/$1.properties
+java -cp lib/json-20080701.jar:lib/grinder.jar net.grinder.Grinder $1.properties
