a4a9c37916a5371d1e9ac3c2f636c1ed064ab510
Kjetil Valle <kjetil.valle@bekk.no>
2012-04-23 20:13:19 +0200

    forbedret eksempel

--- example/script.py
+++ example/script.py
@@ -1,15 +1,20 @@
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
 
-def say_hello():
+# defining a simple "hello world" function, in order to have something to test 
+def hello_world():
     thread = grinder.getThreadNumber()
     print '> worker thread %d: hello world!' % thread
 
 class TestRunner:
     
     def __init__(self):
-        self.test = Test(1, "test description").wrap(say_hello)
+        # creating a Grinder test object
+        test = Test(1, "test description") 
+        # creating a proxy, by wrapping the hello world function
+        self.wrapped_function = test.wrap(hello_world) 
 
     def __call__(self):
-        self.test()
-        
+        # calling "hello world" through the wrapped function
+        self.wrapped_function() 
+
